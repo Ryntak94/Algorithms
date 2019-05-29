8339939b2abbac39cbcb732f9bb31b9bd1470403
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-05-28 20:50:20 -0400

    complete recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,11 +3,20 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    keys = recipe.keys()
+    max_recipe = 1000
+    for i in keys:
+        if max_recipe and i in ingredients:
+            max_recipe = math.floor(min(ingredients[i]/recipe[i], max_recipe))
+        elif i in ingredients:
+            max_recipe = math.floor(ingredients[i]/recipe[i])
+        else:
+            max_recipe = 0
+    return max_recipe
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
