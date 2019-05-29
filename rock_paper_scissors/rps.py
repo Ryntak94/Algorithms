3f79a96f410f190c9135f75b674a315820eb45d2
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-05-29 12:45:33 -0400

    complete rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,9 +3,16 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  results = []
+  possibilities = ["rock", "paper", "scissors"]
 
-
+  def play(playedSoFar, rounds):
+      if rounds == 0:
+          return results.append(playedSoFar)
+      for i in range(0, 3):
+          play(playedSoFar+[possibilities[i]], rounds - 1)
+  play([], n)
+  return results
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_plays = int(sys.argv[1])
